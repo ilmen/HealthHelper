2710bed63661839cc36f878ff558026df16615fa
shaposhnikov <mr.shaposhnikov@mail.ru>
2015-04-02 18:11:33 +0600

    В добавку...

---
+++ HealthWorkHelper/Classes/IScriptManagerSettingProvider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthWorkHelper.Classes
+{
+    public interface IScriptManagerSettingProvider
+    {
+        TimeSpan DelayDuration
+        { get; }
+        TimeSpan RelaxDuration
+        { get; }
+        TimeSpan WorkDuration
+        { get; }
+
+    }
+}
---
+++ HealthWorkHelper/Classes/ScriptManagerSettingProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthWorkHelper.Classes
+{
+    public class ScriptManagerSettingProvider : IScriptManagerSettingProvider
+    {
+        public TimeSpan DelayDuration
+        { get; private set; }
+
+        public TimeSpan RelaxDuration
+        { get; private set; }
+
+        public TimeSpan WorkDuration
+        { get; private set; }
+
+        public ScriptManagerSettingProvider()
+        {
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                this.DelayDuration = TimeSpan.FromSeconds(10);
+                this.RelaxDuration = TimeSpan.FromSeconds(5);
+                this.WorkDuration = TimeSpan.FromSeconds(20);
+            }
+            else
+            {
+                this.DelayDuration = Properties.Settings.Default.DelayDuration;
+                this.RelaxDuration = Properties.Settings.Default.RelaxDuration;
+                this.WorkDuration = Properties.Settings.Default.WorkDuration;
+            }
+        }
+    }
+}
