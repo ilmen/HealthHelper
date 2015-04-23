3743ded099f6239ea2b4f3e3faa1cb74cce82421
shaposhnikov <mr.shaposhnikov@mail.ru>
2015-04-23 08:14:28 +0600

    - [UPD] Added TimeSpanToIntegerConverter for better delay button text

---
+++ HealthWorkHelper/Classes/Converters/TimeSpanToIntegerConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace HealthWorkHelper.Classes.Converters
+{
+    public class TimeSpanToIntegerConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is TimeSpan)) throw new InvalidCastException("Конвертер поддерживает преобразование только TimeSpan!");
+
+            var span = (TimeSpan)value;
+            return Math.Abs(span.TotalMinutes) < 1 ? span.TotalSeconds : span.TotalMinutes;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException("Обратное преобразование не поддерживается!");
+        }
+    }
+}
